e3c4a508b7144f54a68ddf426ed2857c61eeb456
Shanti Naik <visitsb@gmail.com>
2020-06-27 18:54:02 +0900

    Health check support for NLB target groups using goss

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,9 @@ USER root
 COPY keep-alive.sh /usr/local/bin/
 COPY sshd_config /tmp/
 COPY sudoers /tmp/
+COPY goss.yaml /goss.yaml
 
+ENV GOSS_VERSION=v0.3.13
 # Use yum whatprovides to identify which package is suitable for command you want to install
 # yum whatprovides netstat
 # https://stackoverflow.com/a/57073163
@@ -28,6 +30,7 @@ RUN /usr/bin/yum -y upgrade \
  && /usr/bin/yum -y update \
  && /usr/bin/yum -y clean all \
  && /usr/bin/rm -rf /var/cache/yum \
+ && /usr/bin/curl --location --compressed https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/goss-linux-amd64 --output /usr/local/bin/goss \
  && /usr/sbin/useradd --user-group --groups wheel --uid 1000 ec2-user \
  && /bin/echo "root:" | /usr/sbin/chpasswd \
  && /bin/echo "ec2-user:ec2-user" | /usr/sbin/chpasswd \
@@ -35,13 +38,20 @@ RUN /usr/bin/yum -y upgrade \
  && /bin/cat /tmp/sudoers >> /etc/sudoers \
  && /usr/bin/ssh-keygen -A \
  && /usr/bin/systemctl enable sshd \
+ && /bin/chmod ugo+r /goss.yaml \
+ && /bin/chmod ugo+rx /usr/local/bin/goss \
  && /bin/chmod ugo+rx /usr/local/bin/keep-alive.sh
 
 EXPOSE 22/tcp
+
+# http://0.0.0.0:8080/healthz
+EXPOSE 8080/tcp
+HEALTHCHECK --interval=5s --timeout=5s --start-period=5s --retries=3 CMD /usr/local/bin/goss -g /goss.yaml validate
+
+STOPSIGNAL SIGTERM
 
 # root password is empty
 # User "ec2-user" is created with sudo rights (no password required to run sudo commands)
 # Password is "ec2-user"
 USER ec2-user
-
 CMD ["keep-alive.sh"]
--- keep-alive.sh
+++ keep-alive.sh
@@ -1,13 +1,62 @@
 #!/bin/bash
 
-# Start sshd daemon
+/bin/echo "Starting SSH daemon..."
 /usr/bin/sudo /usr/sbin/sshd
+/bin/echo "Starting GOSS health checks..."
+/bin/sh -c "cd / ; /usr/bin/sudo /usr/local/bin/goss serve --format json --cache 5s --listen-addr 0.0.0.0:8080 --endpoint /healthz " 1> /dev/null 2>&1 &
+
+########### SIGINT handler ############
+function _int() {
+  /bin/echo "Stopping SSH daemon..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/sbin/sshd
+
+  /bin/echo "Stopping GOSS health checks..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/local/bin/goss
+
+  /bin/echo "Waking up from sleep..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/bin/sleep
+}
+
+########### SIGTERM handler ############
+function _term() {
+  /bin/echo "Stopping SSH daemon..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/sbin/sshd
+
+  /bin/echo "Stopping GOSS health checks..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/local/bin/goss
+
+  /bin/echo "Waking up from sleep..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/bin/sleep
+}
+
+########### SIGKILL handler ############
+function _kill() {
+  /bin/echo "Stopping SSH daemon..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/sbin/sshd
+
+  /bin/echo "Stopping GOSS health checks..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/local/bin/goss
+
+  /bin/echo "Waking up from sleep..."
+  /usr/bin/sudo /usr/bin/pkill --full /usr/bin/sleep
+}
+
+# Set SIGINT handler
+trap _int SIGINT
+# Set SIGTERM handler
+trap _term SIGTERM
+# Set SIGKILL handler
+trap _kill SIGKILL
 
 while true
-do 
-  echo "Running infinitely using sleep for 60 seconds..."
-  sleep 60
+do
+  /bin/echo "Going to sleep for 60 seconds. Zzz..."
+  /usr/bin/sleep 60s
+  if [ ${?} -ne 0 ]; then
+    /bin/echo "Did someone wake me up?"
+    break
+  fi;
 done
 
-exit $?
-
+/bin/echo "Shutting down..."
+exit ${?}
